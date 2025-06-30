5959f0e59158c3f0ae940e59905e8b70498c2eaa
DELL <DELL@DESKTOP-PL5S8SL>
2025-06-30 14:28:24 +0530

    done

--- src/main/java/AttendanceTrackingSystem/com/attendTrackingSystem/Entity/User.java
+++ src/main/java/AttendanceTrackingSystem/com/attendTrackingSystem/Entity/User.java
@@ -6,5 +6,48 @@ public class User {
 	private String name;
 	private String email;
 	private String role;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int userId, String name, String email, String role) {
+		this.userId = userId;
+		this.name = name;
+		this.email = email;
+		this.role = role;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
