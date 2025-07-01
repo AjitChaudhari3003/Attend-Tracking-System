605c9d22ec6a3024ceb1ee5fa9e497c7b96b6fe5
Sarika <ratkalkar228@gmail.com>
2025-07-01 11:26:52 +0530

    Done

---
+++ src/main/java/AttendanceTrackingSystem/com/attendTrackingSystem/Controller/UserController.java
@@ -0,0 +1,10 @@
+package AttendanceTrackingSystem.com.attendTrackingSystem.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	
+}
+
